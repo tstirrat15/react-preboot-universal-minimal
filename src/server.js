8af98be1d469a562f93ca8a69d5dcc92b2973c20
Tanner Stirrat <tstirrat@gmail.com>
2018-02-27 11:12:27 +0200

    Remove body parsing middleware

--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@
 
 import path from 'path';
 import express from 'express';
-import bodyParser from 'body-parser';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import App from './components/App';
@@ -17,32 +16,18 @@ import Html from './components/Html';
 import assets from './assets.json'; // eslint-disable-line import/no-unresolved
 import config from './config';
 
-process.on('unhandledRejection', (reason, p) => {
-  console.error('Unhandled Rejection at:', p, 'reason:', reason);
-  // send entire app down. Process manager will restart it
-  process.exit(1);
-});
-
-//
-// Tell any CSS tooling (such as Material UI) to use all vendor prefixes if the
-// user agent is not known.
-// -----------------------------------------------------------------------------
-global.navigator = global.navigator || {};
-global.navigator.userAgent = global.navigator.userAgent || 'all';
-
 const app = express();
 
 //
 // Register Node.js middleware
 // -----------------------------------------------------------------------------
 app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 //
 // Register server-side rendering middleware
 // -----------------------------------------------------------------------------
 app.get('*', async (req, res, next) => {
+  console.log('maybe?')
   try {
     const data = {
       children: ReactDOM.renderToString(<App />),
