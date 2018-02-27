71a9fa2f827e37f18430027231bb2c58721acd2d
Tanner Stirrat <tstirrat@gmail.com>
2018-02-27 14:21:46 +0200

    Port over server from different project

--- src/server.js
+++ src/server.js
@@ -1,51 +1,27 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import path from 'path';
 import express from 'express';
-import React from 'react';
-import ReactDOM from 'react-dom/server';
-import App from './components/App';
-import Html from './components/Html';
-import assets from './assets.json'; // eslint-disable-line import/no-unresolved
-import config from './config';
+
+// eslint-disable-next-line import/no-unresolved
+import clientStats from './stats.json';
+import serverRender from './render';
 
 const app = express();
+
+const publicPath = path.join(__dirname, 'public');
+const assetsPath = path.join(publicPath, 'assets');
+const port = process.env.PORT || 3000;
 
 //
 // Register Node.js middleware
 // -----------------------------------------------------------------------------
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use('/assets', express.static(assetsPath));
+app.use(serverRender({ clientStats }));
 
-//
-// Register server-side rendering middleware
-// -----------------------------------------------------------------------------
-app.get('*', async (req, res, next) => {
-  console.log('maybe?')
-  try {
-    const data = {
-      children: ReactDOM.renderToString(<App />),
-      scripts: [assets.vendor.js],
-    };
-
-    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
-    res.status(200);
-    res.send(`<!doctype html>${html}`);
-  } catch (err) {
-    next(err);
-  }
-});
 
 //
 // Launch the server
 // -----------------------------------------------------------------------------
-app.listen(config.port, () => {
-  console.info(`The server is running at http://localhost:${config.port}/`);
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`The server is running at http://localhost:${port}/`);
 });
-export default app;
